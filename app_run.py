ba83bd61c7411852408769b0c2ff07f02bf44043
Mikhail <puryginm@gmail.com>
2019-01-31 09:31:16 +0300

    Lesson 4

--- app_run.py
+++ app_run.py
@@ -1,1 +1,6 @@
-from First_App import app
+from First_App import app, db
+from First_App.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
